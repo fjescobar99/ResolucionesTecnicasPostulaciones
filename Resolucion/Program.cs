d708d41661b6f61ed99ac1879b3841f85b0b00f1
fjescobar99 <45143998+fjescobar99@users.noreply.github.com>
2022-04-27 20:12:04 -0300

    Agregar archivos de proyecto.

---
+++ Resolucion/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Escribir el método 
+
+Int Ocurrencias(int buscado, arreglo int[])
+
+Que devuelva la cantidad de ocurrencias de “buscado” en “arreglo” 
+*/
+/*
+ Escribir un programa en C# que obtenga e imprima a partir de un arreglo de enteros la cantidad de veces que aparece cada elemento usando un asterisco para contar cada aparición del elemento. 
+En el resultado, los elementos deben estar ordenados de menor a mayor. 
+Para el ejercicio considerar elementos entre el 1 y el 9.
+Por ejemplo, para el arreglo: 
+int[] myArray = int[] { 8,1,6,7,8,4,9,8,2 }; el resultado se vería así:
+
+1: *
+2: *
+3:
+4: *
+5: 
+6: *
+7: *
+8: **
+9: *
+*/
+namespace Resolucion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[] { 4, 9, 8, 2 ,3,1,9,4,1,6,5};
+            var arrayOrdenado = OrdenarArrayAsc(myArray);
+            int tamañoArray = arrayOrdenado.Length;
+            for (int i = 1; i < tamañoArray; i++)
+            {
+                Console.Write("\n" + arrayOrdenado[i - 1].ToString() + ": ");
+                Console.Write("*");                
+                while (i < tamañoArray && arrayOrdenado[i - 1] == arrayOrdenado[i])
+                {                    
+                    Console.Write(" *");
+                    i++;
+                }
+            }
+            Console.ReadLine();
+        }
+        private static int[] OrdenarArrayAsc(int[] myArray)
+        {
+            var arrayOrdenado = myArray;
+            for (int i = 0; i < arrayOrdenado.Length; i++)
+            {
+                for (int j = 1; j < arrayOrdenado.Length; j++)
+                {
+                    if (arrayOrdenado[j - 1] > arrayOrdenado[j])
+                    {
+                        int aux = arrayOrdenado[j - 1];
+                        arrayOrdenado[j - 1] = arrayOrdenado[j];
+                        arrayOrdenado[j] = aux;
+                    }
+                }
+            }
+            return arrayOrdenado;
+        }
+
+        private static int[] CopiarArrayExceptoNro(int primerNro, int[] arrayOrdenado)
+        {
+            int ocurrenciasNro = Ocurrencias(primerNro, arrayOrdenado);
+            int tamañoNuevoArray = arrayOrdenado.Length - ocurrenciasNro;
+            int[] arrayNuevo = new int[tamañoNuevoArray];
+            int j = 0;
+            for (int i = tamañoNuevoArray - 1; i < arrayOrdenado.Length; i++)
+            {
+                if (arrayNuevo.Length > 0)
+                {
+                    arrayNuevo[j] = arrayOrdenado[i];
+                    j++;
+                }
+            }
+            return arrayNuevo;
+
+        }
+
+        
+
+        private static void ImprimirOcurrencias(int[] myArray)
+        {
+
+            Console.WriteLine(myArray.Max().ToString());
+            Console.ReadLine();
+        }
+
+        public static int Ocurrencias(int buscado, int[] arreglo)
+        {
+            int contador = 0;
+            for(int i=0; i<arreglo.Length; i++)
+            {
+                if (arreglo[i] == buscado)
+                {
+                    contador++;
+                }
+            }
+            return contador;
+        }
+
+    }
+}
---
+++ Resolucion_grupoSabra/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resolucion_grupoSabra
+{
+    class Program
+    {
+        /*a) Realizar una aplicación de consola que muestre como salida 15 líneas como estas.
+            1
+            12
+            123
+            1234
+            ... (hasta tener las 15 líneas, y en la última tener los 15 dígitos)
+        */
+        static void Main(string[] args)
+        {
+            //MostrarDatos();
+            MostrarDatos_puntoB();
+
+        }
+        /*Realizar una aplicación de consola que ordene un array de números enteros ingresados por
+            parámetro. Y también devolver el promedio.*/
+        private static void MostrarDatos_puntoB()
+        {
+            try
+            {
+                Console.WriteLine("Ingrese el tamaño del array: ");
+                int tamañoArray = int.Parse(Console.ReadLine());
+                var arrayNros = new int[tamañoArray];
+                for (int i = 0; i<tamañoArray; i++)
+                {
+                    Console.WriteLine($"Ingrese valor para la posicion {i.ToString()}: ");
+                    string nroIngresado = Console.ReadLine();
+                    if (int.TryParse(nroIngresado, out arrayNros[i])) 
+                    {
+
+                    }                    
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }            
+        }
+
+        private static void MostrarDatos_puntoA()
+        {
+            string nrosConcatenados = String.Empty;
+            for(int i = 1; i <= 15; i++)
+            {
+                nrosConcatenados += i.ToString();
+                Console.WriteLine(nrosConcatenados);
+            }
+            Console.ReadLine();
+        }
+    }
+}
