a51639c950a642675d25991ae8d30eecb462a720
fjescobar99 <45143998+fjescobar99@users.noreply.github.com>
2022-04-28 08:49:39 -0300

    Se completo el punto 2

--- Resolucion_grupoSabra/Program.cs
+++ Resolucion_grupoSabra/Program.cs
@@ -34,17 +34,67 @@ namespace Resolucion_grupoSabra
                 {
                     Console.WriteLine($"Ingrese valor para la posicion {i.ToString()}: ");
                     string nroIngresado = Console.ReadLine();
-                    if (int.TryParse(nroIngresado, out arrayNros[i])) 
+                    if (!int.TryParse(nroIngresado, out arrayNros[i])) 
                     {
-
+                        i -= 1;
                     }                    
-                }
-
+                }                
+                Console.WriteLine("Array de nros: ");
+                ImprimirArray(arrayNros);
+                OrdenarArray(arrayNros);
+                var promedio = CalcularPromedioDeArray(arrayNros);
+                Console.WriteLine("Array de nros. ordenado:");
+                ImprimirArray(arrayNros);                
+                ImprimirPromedio(promedio);
+                Console.ReadLine();
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Error: " + ex.Message);
             }            
+        }
+
+        private static void ImprimirArray(int[] arrayNros)
+        {
+            string arrayConcatenado = "[";
+            for(int i = 0; i<arrayNros.Length; i++)
+            {
+                arrayConcatenado += arrayNros[i].ToString() + ", ";
+            }
+            arrayConcatenado += "]";
+            Console.WriteLine(arrayConcatenado);
+        }
+
+        private static void ImprimirPromedio(float promedio)
+        {
+            Console.WriteLine("Promedio: " + promedio.ToString());
+        }
+
+        private static float CalcularPromedioDeArray(int[] arrayNros)
+        {
+            float totalNros = 0;
+            for (int i = 0; i < arrayNros.Length; i++)
+            {
+                totalNros += arrayNros[i];
+            }
+            return totalNros / arrayNros.Length;
+        }
+
+        private static void OrdenarArray(int[] arrayNros)
+        {
+            for (int i = 0; i < arrayNros.Length; i++)
+            {
+                for (int j = 1; j < arrayNros.Length; j++)
+                {
+                    if (arrayNros[j] > arrayNros[j - 1])
+                    {
+                        int aux = arrayNros[j];
+                        arrayNros[j] = arrayNros[j - 1];
+                        arrayNros[j - 1] = aux;
+                    }
+                }
+            }
+
         }
 
         private static void MostrarDatos_puntoA()
