533982bac11c3a92b238e4160b72ef15a2a8b044
fjescobar99 <45143998+fjescobar99@users.noreply.github.com>
2022-04-29 14:56:56 -0300

    Se limpio el proyecto porque querian el codigo para poder ejecutarlo.

--- Resolucion_grupoSabra/Properties/AssemblyInfo.cs
+++ Resolucion_grupoSabra/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada a un ensamblado.
-[assembly: AssemblyTitle("Resolucion_grupoSabra")]
+[assembly: AssemblyTitle("PuntoA")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
