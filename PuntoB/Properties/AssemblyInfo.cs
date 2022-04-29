67183af64b3b51305c15c08ca8f55f63760c6a68
fjescobar99 <45143998+fjescobar99@users.noreply.github.com>
2022-04-29 14:54:21 -0300

    Se emprolijo la arq del proyecto para la entrega

--- PuntoB/Properties/AssemblyInfo.cs
+++ PuntoB/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada a un ensamblado.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("PuntoB")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("PuntoB")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("fc5482e1-4624-4eb6-b27d-ef9e17deb7a5")]
+[assembly: Guid("83775a15-ee9b-4ddd-926c-78264b03e358")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
--- Resolucion_grupoSabra/Program.cs
+++ Resolucion_grupoSabra/Program.cs
@@ -16,83 +16,11 @@ namespace Resolucion_grupoSabra
             ... (hasta tener las 15 líneas, y en la última tener los 15 dígitos)
         */
         static void Main(string[] args)
-        {            
-            MostrarDatos_puntoB();
-        }
-
-        private static void MostrarDatos_puntoB()
         {
-            try
-            {
-                Console.WriteLine("Ingrese el tamaño del array: ");
-                int tamañoArray = int.Parse(Console.ReadLine());
-                var arrayNros = new int[tamañoArray];
-                for (int i = 0; i<tamañoArray; i++)
-                {
-                    Console.WriteLine($"Ingrese valor para la posicion {i.ToString()}: ");
-                    string nroIngresado = Console.ReadLine();
-                    if (!int.TryParse(nroIngresado, out arrayNros[i])) 
-                    {
-                        i -= 1;
-                    }                    
-                }                
-                Console.WriteLine("Array de nros: ");
-                ImprimirArray(arrayNros);
-                OrdenarArray(arrayNros);
-                var promedio = CalcularPromedioDeArray(arrayNros);
-                Console.WriteLine("Array de nros. ordenado:");
-                ImprimirArray(arrayNros);                
-                ImprimirPromedio(promedio);
-                Console.ReadLine();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error: " + ex.Message);
-            }            
+            MostrarDatos_puntoA();
         }
 
-        private static void ImprimirArray(int[] arrayNros)
-        {
-            string arrayConcatenado = "[";
-            for(int i = 0; i<arrayNros.Length; i++)
-            {
-                arrayConcatenado += arrayNros[i].ToString() + ", ";
-            }
-            arrayConcatenado += "]";
-            Console.WriteLine(arrayConcatenado);
-        }
-
-        private static void ImprimirPromedio(float promedio)
-        {
-            Console.WriteLine("Promedio: " + promedio.ToString());
-        }
-
-        private static float CalcularPromedioDeArray(int[] arrayNros)
-        {
-            float totalNros = 0;
-            for (int i = 0; i < arrayNros.Length; i++)
-            {
-                totalNros += arrayNros[i];
-            }
-            return totalNros / arrayNros.Length;
-        }
-
-        private static void OrdenarArray(int[] arrayNros)
-        {
-            for (int i = 0; i < arrayNros.Length; i++)
-            {
-                for (int j = 1; j < arrayNros.Length; j++)
-                {
-                    if (arrayNros[j] > arrayNros[j - 1])
-                    {
-                        int aux = arrayNros[j];
-                        arrayNros[j] = arrayNros[j - 1];
-                        arrayNros[j - 1] = aux;
-                    }
-                }
-            }
-
-        }
+       
 
         private static void MostrarDatos_puntoA()
         {
